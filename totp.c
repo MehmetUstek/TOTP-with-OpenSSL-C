824a1165f4cb0dc85f7f1df35cb95abd0bdea788
Mehmet Ustek <mustek17@ku.edu.tr>
2023-10-14 23:25:45 +0100

    First Iteration

--- totp.c
+++ totp.c
@@ -8,12 +8,13 @@
 #include <stdlib.h>
 
 // Function declarations
-void hexToByteArray(const char *hex, unsigned char **byteArray, size_t *length);
+void longToHex(time_t unix_time, int64_t T0, int64_t X, char *steps);
+void hexToByteArray(const char *hex, unsigned char *byteArray, size_t length);
 unsigned char *mx_hmac_sha256(const void *key, int keylen, const unsigned char *data, int datalen,
                               unsigned char *result, unsigned int *resultlen);
 unsigned char *hmac_result(unsigned char *key, const unsigned char *msg);
-char *strPaddingToBeginning(char *str);
-char *generateTOTP(char *key, char time[16], int codeDigits);
+void generateTOTP(char *key, time_t time, int codeDigits, int64_t T0, int64_t X,
+                  char result[7]);
 
 static int DIGITS_POWER[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
 long testTime[] = {59L, 1111111109L, 1111111111L,
@@ -47,43 +48,17 @@ unsigned char *hmac_result(unsigned char *key, const unsigned char *msg)
     return result;
 }
 
-// TODO: Test this function
-char *strPaddingToBeginning(char *str)
+void generateTOTP(char *key,
+                  time_t time, // I choose not to implement time as a hex string as it is on the documentation, because I find it more confusing.
+                  int codeDigits, int64_t T0, int64_t X,
+                  char result[7])
 {
-    size_t len = strlen(str);
-    memmove(str + 1, str, ++len);
-    *str = '0';
-    return str;
-}
-
-char *generateTOTP(char *key,
-                   char time[8],
-                   int codeDigits)
-{
-    char *result;
-    // Using the counter
-    // First 8 bytes are for the movingFactor
-    // Compliant with base RFC 4226 (HOTP)
-    // int targetStrLen = 16;
-    // const char *padding = "0000000000000000";
-
-    // int timeByteDifference = targetStrLen - strlen(time);
-    // if (timeByteDifference < 0)
-    //     timeByteDifference = 0;
-    // sprintf(time, "[%*.*s%s]", timeByteDifference, timeByteDifference, padding, time);
-
-    // while (strlen(time) < 16)
-    // {
-    //     // time = "0" + time;
-    //     size_t len = strlen(time);
-    //     memmove(time + 1, time, ++len);
-    //     *time = '0';
-    // }
+    char steps[17]; // This steps replaces the 'time' variable in the documentation.
+    longToHex(time, T0, X, steps);
     // Get the HEX in a Byte[]
-    // TODO: Probably will not work bc. of the []. Change it to *
     size_t msglen;
     unsigned char *msg;
-    hexToByteArray(time, msg, msglen);
+    hexToByteArray(steps, msg, msglen);
 
     size_t klen;
     unsigned char *k;
@@ -97,25 +72,15 @@ char *generateTOTP(char *key,
         ((hash[offset + 2] & 0xff) << 8) |
         (hash[offset + 3] & 0xff);
     int otp = binary % DIGITS_POWER[codeDigits];
-    sprintf(result, "%d", otp);
-    while (strlen(result) < codeDigits)
-    {
-        strPaddingToBeginning(result);
-    }
-    free(k);
-    return result;
+    snprintf(result, 7, "%06d", otp);
 }
 
-void longToHex(int64_t unix_time, int64_t T0, int64_t X, char *steps)
+void longToHex(time_t unix_time, int64_t T0, int64_t X, char *steps)
 {
     int64_t T = (unix_time - T0) / X;
 
     // zero-padding to fill 16 bytes, convert from long to hexadecimal.
-    snprintf(steps, 16, "%016lX", T);
-    for (int i = 0; steps[i]; i++)
-    {
-        steps[i] = toupper(steps[i]);
-    }
+    snprintf(steps, 17, "%016lX", T);
 
     printf("Hexadecimal string: %s\n", steps);
 }
@@ -124,8 +89,10 @@ void longToHex(int64_t unix_time, int64_t T0, int64_t X, char *steps)
 /// This code was brought and modified from stackoverflow.
 /// @param hex
 /// @return byte array
-void hexToByteArray(const char *hex, unsigned char **byteArray, size_t *length)
+void hexToByteArray(const char *hex, unsigned char *byteArray, size_t length)
 {
+    length = (strlen(hex) + 1) / 2;
+
     // Concatenate "10" with the input hex string
     char *inputHex = malloc(strlen(hex) + 3); // 2 for "10", 1 for null terminator
     strcpy(inputHex, "10");
@@ -133,19 +100,22 @@ void hexToByteArray(const char *hex, unsigned char **byteArray, size_t *length)
 
     // Convert hex string to unsigned long long integer
     unsigned long long num = strtoull(inputHex, NULL, 16);
-
-    // Determine the length of the byte array
-    *length = (size_t)((sizeof(unsigned long long) * 2 + 1) / 3); // Each byte represented by 2 hex characters, plus 1 for null terminator
-
     // Allocate memory for the byte array
-    *byteArray = malloc(*length);
+    byteArray = (unsigned char *)malloc(length);
 
-    // Copy the integer to the byte array
-    for (size_t i = 0; i < *length; ++i)
+    for (size_t i = 0; i < length; ++i)
     {
-        (*byteArray)[i] = (unsigned char)(num >> (8 * ((*length - 1) - i))); // Extracting individual bytes
+        byteArray[i] = (unsigned char)(num >> (8 * ((length - 1) - i))); // Extracting individual bytes
     }
 
+    // Check if the string has at least one character
+    if (strlen(byteArray) > 1)
+    {
+        // Shift characters to remove the first character
+        memmove(byteArray, byteArray + 1, strlen(byteArray));
+        // Make sure the string is null-terminated
+        byteArray[strlen(byteArray) - 1] = '\0';
+    }
     // Free dynamically allocated memory
     free(inputHex);
 }
@@ -156,56 +126,46 @@ int main(int argc, char **argv)
     signal(SIGINT, sig_handler);
 
     // Seed for HMAC-SHA256 - 32 bytes
-    char *seed32 = "3132333435363738393031323334353637383930313233343536373839303132";
+    char *seed32 = "12345678901234567890";
 
     time_t start;
-    double time_step = 10.0;
+    double time_step = 5.0;
     double time_spent;
     double last_time = 0.0;
     time(&start);
-    // printf("%ld", start);
-    // printf("bytes: %d", sizeof(start));
 
     int64_t testTime[] = {59L};
     int64_t T0 = 0;
     int64_t X = 30;
     time_t unix_time = 1111111109L;
-    char steps[16];
-    longToHex(unix_time, T0, X, &steps);
-    printf("Hexadecimal string: %s\n", steps);
+    char steps[17];
 
-    // printf("Generated OTP:%s", generateTOTP(seed32, start, 6));
-    // const long *hex = 59; // Example input hex string
-    // unsigned char *byteArray;
-    // size_t length;
-    // hexToByteArray(hex, &byteArray, &length);
-    // // Print the resulting byte array
-    // printf("Byte Array: ");
-    // for (size_t i = 0; i < length; ++i)
-    // {
-    //     printf("%02X ", byteArray[i]); // Print in hexadecimal format
-    // }
-    // printf("\n");
-
-    // start = clock();
-    // printf("clock: %ld", start);
+    char result[7];
+    generateTOTP(seed32, unix_time, 6, T0, X, result);
+    printf("TOTP: %s\n\n", result);
 
     /* Mark beginning time */
-    // while (keepRunning)
-    // {
-    //     start = clock();
-    //     do
-    //     {
-    //         /* Get CPU time since loop started */
-    //         time_spent = (double)(clock() - start) / CLOCKS_PER_SEC;
-    //         if (time_spent - last_time >= 1.0)
-    //         {
-    //             // Clean the console and write the new remaining value.
-    //             printf("\033[A\33[2K\r Remaining time: %d seconds\n", (int)round(time_step - time_spent));
-    //             last_time = time_spent;
-    //         }
-    //     } while ((time_spent < time_step) && keepRunning);
-    // }
-    // puts("\nStopped by signal `SIGINT'");
+    while (keepRunning)
+    {
+        start = clock();
+        do
+        {
+            /* Get CPU time since loop started */
+            time_spent = (double)(clock() - start) / CLOCKS_PER_SEC;
+            if (time_spent - last_time >= 1.0)
+            {
+                // Clean the console and write the new remaining value.
+                printf("");
+                printf("\033[A\33[2K\r Remaining time: %d seconds\n", (int)round(time_step - time_spent));
+                last_time = time_spent;
+            }
+        } while ((time_spent < time_step) && keepRunning);
+        // Generate a new TOTP, and continue.
+        strcpy(steps, "");
+        strcpy(result, "");
+        generateTOTP(seed32, unix_time, 6, T0, X, result);
+        time_spent = 0.0;
+    }
+    puts("\nStopped by signal `SIGINT'");
     return EXIT_SUCCESS;
 }
