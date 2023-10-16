d5019f15bffc5e5f8e1b923d6153c6661836d4f7
Mehmet Ustek <mustek17@ku.edu.tr>
2023-10-16 15:13:16 +0100

    Backup

--- totp.c
+++ totp.c
@@ -10,16 +10,15 @@
 // Function declarations
 void longToHex(time_t unix_time, int64_t T0, int64_t X, char *steps);
 void hexToByteArray(const char *hex, unsigned char *val);
-unsigned char *mx_hmac_sha256(const void *key, int keylen, const unsigned char *data, int datalen,
-                              unsigned char *result, unsigned int *resultlen);
-unsigned char *hmac_result(unsigned char *key, const unsigned char *msg, int keylen, int msglen);
+unsigned char *hmac_sha256(const void *key, size_t keylen, const unsigned char *data, size_t datalen,
+                           unsigned char *result, unsigned int *resultlen);
+unsigned char *hmac_result(unsigned char *key, const unsigned char *msg, size_t keylen, size_t msglen);
 void generateTOTP(char *key, time_t time, int codeDigits, int64_t T0, int64_t X,
                   char result[7]);
 
 static int DIGITS_POWER[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
 long testTime[] = {59L, 1111111109L, 1111111111L,
                    1234567890L, 2000000000L, 20000000000L};
-char *steps = "0";
 
 static volatile sig_atomic_t keepRunning = 1;
 
@@ -29,20 +28,23 @@ static void sig_handler(int _)
     keepRunning = 0;
 }
 
-unsigned char *mx_hmac_sha256(const void *key, int keylen,
-                              const unsigned char *msg, int datalen,
-                              unsigned char *result, unsigned int *resultlen)
+unsigned char *hmac_sha256(const void *key, size_t keylen,
+                           const unsigned char *msg, size_t datalen,
+                           unsigned char *result, unsigned int *resultlen)
 {
     return HMAC(EVP_sha256(), key, keylen, msg, datalen, result, resultlen);
 }
 
 // TODO: Error Handling
-unsigned char *hmac_result(unsigned char *key, const unsigned char *msg, int keylen, int msglen)
+unsigned char *hmac_result(unsigned char *key, const unsigned char *msg, size_t keylen, size_t msglen)
 {
+    // int length = sizeof(msg) / sizeof(msg[0]);
+    // int lkength = sizeof(key) / sizeof(key[0]);
+    // size_t keykklen = strlen(key);
     unsigned char *result = NULL;
     unsigned int resultlen = -1;
 
-    result = mx_hmac_sha256((const void *)key, keylen, msg, msglen, result, &resultlen);
+    result = hmac_sha256((const void *)key, keylen, msg, msglen, result, &resultlen);
     return result;
 }
 
@@ -63,29 +65,18 @@ void longToHex(time_t unix_time, int64_t T0, int64_t X, char *steps)
 /// https://stackoverflow.com/questions/3408706/hexadecimal-string-to-byte-array-in-c
 void hexToByteArray(const char *hex, unsigned char *val)
 {
+    // char *inputHex = malloc(strlen(hex) + 3); // 2 for "0x", 1 for null terminator
 
-    // Concatenate "10" with the input hex string
-    char *inputHex = malloc(strlen(hex) + 3); // 2 for "10", 1 for null terminator
+    // strcpy(inputHex, "0x");
+    // strcat(inputHex, hex);
 
-    strcpy(inputHex, "0x");
-    strcat(inputHex, hex);
-    // val = (unsigned char *)calloc(length, sizeof(char));
-    int startIndex = 0;
-    while (hex[startIndex] == '0' && startIndex < strlen(hex) - 1)
-    {
-        startIndex++; // Skip leading zeros
-    }
-    if ((strlen(hex) - startIndex) % 2 != 0)
-    {
-        startIndex--;
-    }
-
-    const char *pos = inputHex;
-    if (val == NULL)
-    {
-        free(inputHex);
-    }
-    size_t length = strlen(inputHex) / 2;
+    // const char *pos = inputHex + 2;
+    // if (val == NULL)
+    // {
+    //     free(inputHex);
+    // }
+    const char *pos = hex;
+    size_t length = strlen(hex) / 2;
 
     for (size_t count = 0; count < length; count++)
     {
@@ -101,7 +92,7 @@ void hexToByteArray(const char *hex, unsigned char *val)
     //     }
     // }
 
-    free(inputHex);
+    // free(inputHex);
 }
 
 void verifyTOTP()
@@ -116,15 +107,15 @@ void generateTOTP(char *key,
     char steps[17]; // This steps replaces the 'time' variable in the documentation.
     longToHex(time, T0, X, steps);
     // Get the HEX in a Byte[]
-    int msglen = (strlen(steps) + 3) / 2;
+    size_t msglen = ((strlen(steps) + 1) / 2); // Could be +1 not sure.
     unsigned char *msg = (unsigned char *)malloc(msglen);
     hexToByteArray(steps, msg);
-    for (size_t i = 0; i < (strlen(steps) + 3) / 2; i++)
-    {
-        printf("msg: %02x ", msg[i]);
-    }
+    // for (size_t i = 0; i < (strlen(steps) + 3) / 2; i++)
+    // {
+    //     printf("msg: %02x ", msg[i]);
+    // }
 
-    int keylen = (strlen(steps) + 3) / 2;
+    size_t keylen = ((strlen(key) + 1) / 2);
     unsigned char *k = (unsigned char *)malloc(keylen);
     hexToByteArray(key, k);
     const unsigned char *hash = hmac_result(k, msg, keylen, msglen);
@@ -139,74 +130,84 @@ void generateTOTP(char *key,
     snprintf(result, 7, "%06d", otp);
 }
 
-char *createDynamicString()
+/// @brief Test with the values given in the last page of rfc6238 paper.
+void test()
 {
-    // Allocate memory for the string
-    char *str = (char *)malloc(50 * sizeof(char)); // Allocate space for 50 characters
-
-    if (str != NULL)
+    // Seed for HMAC-SHA256 - 32 bytes
+    char *secret = "12345678901234567890";
+    long testTime[] = {59L, 1111111109L, 1111111111L, 1234567890L, 2000000000L, 20000000000L};
+    int64_t T0 = 0;
+    int64_t X = 30;
+    for (int i = 0; i < 6; i++)
     {
-        // Copy the content to the dynamically allocated string
-        strcpy(str, "Hello, World!");
+        char result[7];
+        generateTOTP(secret, testTime[i], 6, T0, X, result);
+        printf("TOTP result:%s\n", result);
     }
+}
 
-    return str;
+/// @brief Accept signal to stop terminal with SIGINT (ctrl +c) process.
+void catchSignal()
+{
+    signal(SIGINT, sig_handler);
 }
 
 int main(int argc, char **argv)
 {
-    createDynamicString();
-    // Accept signal to stop terminal with SIGINT (ctrl +c) process.
-    signal(SIGINT, sig_handler);
-
-    // Seed for HMAC-SHA256 - 32 bytes
-    char *seed32 = "12345678901234567890";
-
-    time_t start;
-    double time_step = 5.0;
-    double time_spent;
-    double last_time = 0.0;
-    time(&start);
-
-    int64_t testTime[] = {59L};
-    int64_t T0 = 0;
-    int64_t X = 5;
-    time_t unix_time = 1111111109L;
-    char steps[17];
-    printf("Current Time as unix time:%ld\n", start);
-
-    char result[7];
-    generateTOTP(seed32, unix_time, 6, T0, X, result);
-    time_t currentTime;
-
-    /* Mark beginning time */
-    while (keepRunning)
+    catchSignal();
+    if (argc > 1)
     {
-        time(&start);
-        do
-        {
-            time(&currentTime);
-            /* Get CPU time since loop started */
-            time_spent = (double)difftime(currentTime, start);
-            if (time_spent - last_time >= 1.0)
-            {
-                // Clean the console and write the new remaining value.
-                printf("");
-                printf(" Remaining time: %d seconds\t Your TOTP:%s\n", (int)round(time_step - time_spent), result);
-                //\033[A\33[2K\r\033[A\33[2K\r
-                last_time = time_spent;
-                verifyTOTP();
-            }
-        } while ((time_spent < time_step) && keepRunning);
-        // Generate a new TOTP, and continue.
-        strcpy(steps, "");
-        strcpy(result, "");
-        time(&currentTime);
-        // printf("Current Time: %ld", currentTime);
-        generateTOTP(seed32, currentTime, 6, T0, X, result);
-        time_spent = 0.0;
-        last_time = 0.0;
+        if (strcmp(argv[1], "test") == 0)
+            test();
     }
-    puts("\nStopped by signal `SIGINT'");
+    else
+    {
+        // Seed for HMAC-SHA256 - 32 bytes
+        char *seed32 = "12345678901234567890";
+
+        time_t start;
+        double time_step = 30.0;
+        double time_spent;
+        double last_time = 0.0;
+        time(&start);
+
+        int64_t T0 = 0;
+        int64_t X = 30;
+        printf("Current Time as unix time:%ld\n", start);
+
+        char result[7];
+        // generateTOTP(seed32, unix_time, 6, T0, X, result);
+        time_t currentTime;
+        time(&currentTime);
+
+        /* Mark beginning time */
+        while (keepRunning)
+        {
+            time(&start);
+            generateTOTP(seed32, 1111111109L, 6, T0, X, result);
+            do
+            {
+                time(&currentTime);
+                /* Get CPU time since loop started */
+                time_spent = (double)difftime(currentTime, start);
+                if (time_spent - last_time >= 1.0)
+                {
+                    // Clean the console and write the new remaining value.
+                    printf("");
+                    printf("\033[A\33[2K\r\033[A\33[2K\r Remaining time: %d seconds\t Your TOTP:%s\n", (int)round(time_step - time_spent), result);
+                    //\033[A\33[2K\r\033[A\33[2K\r
+                    last_time = time_spent;
+                    verifyTOTP();
+                }
+            } while ((time_spent < time_step) && keepRunning);
+            // Generate a new TOTP, and continue.
+            strcpy(result, "");
+            time(&currentTime);
+            // printf("Current Time: %ld", currentTime);
+            time_spent = 0.0;
+            last_time = 0.0;
+        }
+        puts("\nStopped by signal `SIGINT'");
+    }
     return EXIT_SUCCESS;
 }
